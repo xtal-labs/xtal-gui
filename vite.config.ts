fd3c9af280b50423c409ab2096474a5860b027fe
Christian Mate <41514384+cm8t@users.noreply.github.com>
2026-05-15 01:58:11 -0500

    (update): migrate to node v26

--- vite.config.ts
+++ vite.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
   },
   envPrefix: ['VITE_', 'TAURI_'],
   build: {
-    target: ['es2021', 'chrome100', 'safari13'],
+    target: 'baseline-widely-available',
     minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
     sourcemap: !!process.env.TAURI_DEBUG,
     rollupOptions: {
